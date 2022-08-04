17b38024494843dc425e951586ba7255be7ea457
Misha <94810441+Miwa9600@users.noreply.github.com>
2022-08-05 01:05:08 +0500

    ChessMain & Player

--- src/main/java/ChessMain.java
+++ src/main/java/ChessMain.java
@@ -1,6 +1,9 @@
 public class ChessMain {
 
     public static void main (String[] args) {
+        Player whitePlayer = new Player("Beth Harmon", "beth.harmon@gmail.com", true,2000, 20);
+        Player blackPlayer = new Player("Vasiliy Borgov", "vasiliy.borgov@gmail.com", false, 2500, 45);
+        System.out.println(whitePlayer);
 
 
     }
